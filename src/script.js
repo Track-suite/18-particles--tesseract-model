861107f214422ca4980037a2cce32f8750c3ed1a
tracksuite <youngunner1998@gmail.com>
2021-12-06 11:02:40 +0530

    completed the lesson 19

--- src/script.js
+++ src/script.js
@@ -20,47 +20,78 @@ const scene = new THREE.Scene()
  */
 const textureLoader = new THREE.TextureLoader()
 
-const particleTexture = textureLoader.load('/textures/particles/9.png')
+const particleTexture = textureLoader.load('/textures/particles/1.png')
+const particleTexture1 = textureLoader.load('/textures/particles/9.png')
 const boxTexture = textureLoader.load('/textures/particles/9.png')
 
 /**
  * Particles
  */
 //cube 
- const cube = new THREE.Mesh(
-    new THREE.SphereGeometry(1,5,16),
-    new THREE.MeshBasicMaterial({
-        normalMap: boxTexture,
-        transparent: false
+const cube = new THREE.SphereBufferGeometry(1,6,8,1)
+//  const cube = new THREE.Mesh(
+//     new THREE.SphereBufferGeometry(1,8,2),
+//     // new THREE.MeshBasicMaterial({
+//     //     normalMap: boxTexture,
+//     //     transparent: false
         
-    })
+//     // })
+// )
+
+const Boxmaterial = new THREE.PointsMaterial({
+
+    size:0.5,
+    color:0xffffff,
+    sizeAttenuation: true
+}
 )
+Boxmaterial.alphaMap = particleTexture1
+Boxmaterial.transparent = true
+Boxmaterial.depthTest = false
+Boxmaterial.depthWrite = false
+Boxmaterial.blending = THREE.AdditiveBlending
 
-scene.add(cube)
+const cube2 = new THREE.Points(cube,Boxmaterial)
+ scene.add(cube2)
 
+ 
 // Geometry
 const particlesGeometry = new THREE.BufferGeometry()
-const count = 120000
+const count = 20000
+
+
 
 const positions = new Float32Array(count * 3) // Multiply by 3 because each position is composed of 3 values (x, y, z)
+const colors = new Float32Array(count * 3)
+
 
 for(let i = 0; i < count * 3; i++) // Multiply by 3 for same reason
 {
+    
     positions[i] = (Math.random() - 0.5) * 10 // Math.random() - 0.5 to have a random value between -0.5 and +0.5
+    colors[i] = Math.random()//random colors
 }
 
 particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3)) 
 // Create the Three.js BufferAttribute and specify that each information is composed of 3 values
 
+//particle2.setAttribute('position', new THREE.BufferAttribute(positions,3))//this is my particle added to check 
+
+particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))//random color set
+
 // Material
 const particlesMaterial = new THREE.PointsMaterial({
-    size: 5,
+    size: 0.3,
     sizeAttenuation: true
     
 })
 
-particlesMaterial.size = 0.3
-particlesMaterial.color = new THREE.Color('#3498db')
+//const 
+
+particlesMaterial.vertexColors = true
+
+particlesMaterial.size = 0.2
+//particlesMaterial.color = new THREE.Color('#3495db')
 // particlesMaterial.map = particleTexture
 particlesMaterial.transparent = true
 particlesMaterial.alphaMap = particleTexture
@@ -101,8 +132,8 @@ window.addEventListener('resize', () =>
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.2, 100)
+camera.position.z = 8
 scene.add(camera)
 
 // Controls
@@ -130,6 +161,27 @@ const tick = () =>
     // Update controls
     controls.update()
 
+    // Update particles
+
+    for(let i = 0; i < count; i++)
+    {
+        const i3 = i * 3
+
+        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime)
+    }
+    particlesGeometry.attributes.position.needsUpdate = true 
+
+
+    for(let i = 0; i < count; i++)
+    {
+        let i3 = i * 3
+
+        const x = particlesGeometry.attributes.position.array[i3]
+        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x)
+    }
+    particlesGeometry.attributes.position.needsUpdate = true
+
+    //particles.rotation.y = elapsedTime * 0.2
     // Render
     renderer.render(scene, camera)
 
